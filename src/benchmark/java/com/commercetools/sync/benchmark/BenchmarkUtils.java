ae468164b99d13eb6723396a5af2e46b4515b7db
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-17 09:49:11 +0100

    #246: Refactor benchmarks according to new data.

--- src/benchmark/java/com/commercetools/sync/benchmark/BenchmarkUtils.java
+++ src/benchmark/java/com/commercetools/sync/benchmark/BenchmarkUtils.java
@@ -1,5 +1,6 @@
 package com.commercetools.sync.benchmark;
 
+import com.commercetools.sync.benchmark.helpers.CtpObserver;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
@@ -11,9 +12,9 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Spliterator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -22,75 +23,99 @@ import static java.util.Optional.ofNullable;
 import static java.util.Spliterators.spliteratorUnknownSize;
 import static java.util.stream.StreamSupport.stream;
 
-public class BenchmarkUtils {
+class BenchmarkUtils {
     private static final String BENCHMARK_RESULTS_FILE_NAME = "benchmarks.json";
     private static final String BENCHMARK_RESULTS_FILE_DIR = ofNullable(System.getenv("CI_BUILD_DIR"))
         .map(path -> path + "/tmp_git_dir/benchmarks/").orElse("");
     private static final String BENCHMARK_RESULTS_FILE_PATH = BENCHMARK_RESULTS_FILE_DIR + BENCHMARK_RESULTS_FILE_NAME;
     private static final Charset UTF8_CHARSET = StandardCharsets.UTF_8;
-    private static final String EXECUTION_TIMES = "executionTimes";
-    private static final String AVERAGE = "average";
-    private static final String DIFF = "diff";
+
+    private static final String SYNC_TOTAL_DURATION = "sync_total_duration";
+    private static final String TOTAL_CTP_EXCHANGE_DURATION = "total_ctp_exchange_duration";
+    private static final String AVG_CTP_EXCHANGE_DURATION = "avg_ctp_exchange_duration";
+    private static final String NUMBER_OF_REQUESTS = "number_of_requests";
+    private static final String NUMBER_OF_POSTS = "number_of_POSTs";
+    private static final String NUMBER_OF_GETS = "number_of_GETs";
+    private static final String NUMBER_OF_CREATES = "number_of_CREATES";
+    private static final String NUMBER_OF_UPDATES = "number_of_UPDATES";
+    private static final String NUMBER_OF_QUERIES = "number_of_QUERIES";
 
     static final String PRODUCT_SYNC = "productSync";
     static final String INVENTORY_SYNC = "inventorySync";
     static final String CATEGORY_SYNC = "categorySync";
     static final String TYPE_SYNC = "typeSync";
     static final String PRODUCT_TYPE_SYNC = "productTypeSync";
+
     static final String CREATES_ONLY = "createsOnly";
     static final String UPDATES_ONLY = "updatesOnly";
     static final String CREATES_AND_UPDATES = "mix";
-    static final int THRESHOLD = 120000; //120 seconds in milliseconds
-    static final int NUMBER_OF_RESOURCE_UNDER_TEST = 10000;
+
+    static final int THRESHOLD = 120000; //120 seconds in milliseconds //tODO: WILL CHANGE
+    static final int NUMBER_OF_RESOURCE_UNDER_TEST = 1000;
+
+    private static final String CURRENT_BRANCH = ofNullable(System.getenv("TRAVIS_BRANCH")).orElse("dev");
 
 
-    static void saveNewResult(@Nonnull final String version,
-                              @Nonnull final String sync,
+    static void saveNewResult(@Nonnull final String sync,
                               @Nonnull final String benchmark,
-                              final double newResult) throws IOException {
+                              final double totalExecutionTime,
+                              @Nonnull final CtpObserver ctpObserver) throws IOException {
 
         final JsonNode rootNode = new ObjectMapper().readTree(getFileContent(BENCHMARK_RESULTS_FILE_PATH));
-        final JsonNode withNewResult = addNewResult(rootNode, version, sync, benchmark, newResult);
-        writeToFile(withNewResult.toString(), BENCHMARK_RESULTS_FILE_PATH);
+        addNewResult(rootNode, sync, benchmark, totalExecutionTime, ctpObserver);
+        writeToFile(rootNode.toString(), BENCHMARK_RESULTS_FILE_PATH);
     }
 
-    private static JsonNode addNewResult(@Nonnull final JsonNode originalRoot,
-                                         @Nonnull final String version,
+    private static void addNewResult(@Nonnull final JsonNode originalRoot,
                                          @Nonnull final String sync,
                                          @Nonnull final String benchmark,
-                                         final double newResult) {
-        ObjectNode rootNode = (ObjectNode) originalRoot;
-        ObjectNode versionNode = (ObjectNode) rootNode.get(version);
+                                         final double totalExecutionTime,
+                                         @Nonnull final CtpObserver ctpObserver) {
+
+        ObjectNode rootNodeChildren = (ObjectNode) originalRoot;
+        ObjectNode branchNodeChildren = (ObjectNode) rootNodeChildren.get(CURRENT_BRANCH);
 
         // If version doesn't exist yet, create a new JSON object for the new version.
-        if (versionNode == null) {
-            rootNode = createVersionNode(rootNode, version);
-            versionNode = (ObjectNode) rootNode.get(version);
+        if (branchNodeChildren == null) {
+            branchNodeChildren = createBranchNodeChildren();
+            rootNodeChildren.set(CURRENT_BRANCH, branchNodeChildren);
         }
 
-        final ObjectNode syncNode = (ObjectNode) versionNode.get(sync);
-        final ObjectNode benchmarkNode = (ObjectNode) syncNode.get(benchmark);
+        final ObjectNode syncNodeChildren = (ObjectNode) branchNodeChildren.get(sync);
+        final ObjectNode benchmarkNodeChildren = (ObjectNode) syncNodeChildren.get(benchmark);
 
-        // Get current list of execution times for the specified benchmark of the specified sync module
-        // of the specified version.
-        final List<JsonNode> results = toList(benchmarkNode.get(EXECUTION_TIMES).elements());
+        benchmarkNodeChildren.set(SYNC_TOTAL_DURATION, JsonNodeFactory.instance.numberNode(totalExecutionTime));
+        benchmarkNodeChildren.set(TOTAL_CTP_EXCHANGE_DURATION, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalDuration().longValue()));
+        benchmarkNodeChildren.set(AVG_CTP_EXCHANGE_DURATION, JsonNodeFactory.instance.numberNode(ctpObserver.getAverageDuration().longValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_REQUESTS, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfRequests().intValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_POSTS, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfPosts().intValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_GETS, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfGets().intValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_CREATES, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfCreateRequests().intValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_UPDATES, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfUpdateRequests().intValue()));
+        benchmarkNodeChildren.set(NUMBER_OF_QUERIES, JsonNodeFactory.instance.numberNode(ctpObserver.getTotalNumberOfQueryRequests().intValue()));
+        branchNodeChildren.set(AVG_CTP_EXCHANGE_DURATION, JsonNodeFactory.instance.numberNode(getBranchAvgCtpDuration(branchNodeChildren)));
 
-        // Add new result.
-        results.add(JsonNodeFactory.instance.numberNode(newResult));
-        benchmarkNode.set(EXECUTION_TIMES, JsonNodeFactory.instance.arrayNode().addAll(results));
 
-        // Compute new average and add to JSON Object
-        final double averageResult = calculateAvg(results);
-        benchmarkNode.set(AVERAGE, JsonNodeFactory.instance.numberNode(averageResult));
+    }
 
-        // Compute new diff from the last version.
-        final double diff = calculateDiff(rootNode, version, sync, benchmark, averageResult);
-        benchmarkNode.set(DIFF, JsonNodeFactory.instance.numberNode(diff));
+    private static long getBranchAvgCtpDuration(@Nonnull final JsonNode branchNode) {
+        final long totalAvgCtpDuration = branchNode.get(PRODUCT_SYNC).get(UPDATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(PRODUCT_SYNC).get(CREATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(PRODUCT_SYNC).get(CREATES_AND_UPDATES).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(CATEGORY_SYNC).get(UPDATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(CATEGORY_SYNC).get(CREATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(CATEGORY_SYNC).get(CREATES_AND_UPDATES).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(INVENTORY_SYNC).get(UPDATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(INVENTORY_SYNC).get(CREATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(INVENTORY_SYNC).get(CREATES_AND_UPDATES).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(TYPE_SYNC).get(UPDATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(TYPE_SYNC).get(CREATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(TYPE_SYNC).get(CREATES_AND_UPDATES).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(PRODUCT_TYPE_SYNC).get(UPDATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(PRODUCT_TYPE_SYNC).get(CREATES_ONLY).get(AVG_CTP_EXCHANGE_DURATION).asLong() +
+            branchNode.get(PRODUCT_TYPE_SYNC).get(CREATES_AND_UPDATES).get(AVG_CTP_EXCHANGE_DURATION).asLong();
 
-        final JsonNode newSyncNode = syncNode.set(benchmark, benchmarkNode);
-        final JsonNode newVersionNode = versionNode.set(sync, newSyncNode);
-        final JsonNode newRoot = rootNode.set(version, newVersionNode);
-        return newRoot;
+        return totalAvgCtpDuration / 15;
     }
 
     @Nonnull
@@ -107,63 +132,41 @@ public class BenchmarkUtils {
         return results.stream().mapToDouble(JsonNode::asLong).average().orElse(0);
     }
 
-    private static ObjectNode createVersionNode(@Nonnull final ObjectNode rootNode, @Nonnull final String version) {
-        final ObjectNode newVersionNode = createSyncNode(createSyncNode(createSyncNode(
-            createSyncNode(createSyncNode(JsonNodeFactory.instance.objectNode(),
-                PRODUCT_SYNC), INVENTORY_SYNC), CATEGORY_SYNC), PRODUCT_TYPE_SYNC), TYPE_SYNC);
-        return (ObjectNode) rootNode.set(version, newVersionNode);
+    private static ObjectNode createBranchNodeChildren() {
+
+        final HashMap<String, ObjectNode> nodes = new HashMap<>();
+        nodes.put(PRODUCT_SYNC, createSyncNodeChildren());
+        nodes.put(INVENTORY_SYNC, createSyncNodeChildren());
+        nodes.put(CATEGORY_SYNC, createSyncNodeChildren());
+        nodes.put(PRODUCT_TYPE_SYNC, createSyncNodeChildren());
+        nodes.put(TYPE_SYNC, createSyncNodeChildren());
+
+        return (ObjectNode) JsonNodeFactory.instance.objectNode().setAll(nodes);
     }
 
-    private static ObjectNode createSyncNode(@Nonnull final ObjectNode versionNode,
-                                             @Nonnull final String sync) {
-        final ObjectNode newSyncNode = createBenchmarkNode(
-            createBenchmarkNode(
-                createBenchmarkNode(JsonNodeFactory.instance.objectNode(), CREATES_ONLY), UPDATES_ONLY),
-            CREATES_AND_UPDATES);
-        return (ObjectNode) versionNode.set(sync, newSyncNode);
+    private static ObjectNode createSyncNodeChildren() {
+
+        final HashMap<String, ObjectNode> nodes = new HashMap<>();
+        nodes.put(CREATES_ONLY, createBenchmarkNodeChildren());
+        nodes.put(UPDATES_ONLY, createBenchmarkNodeChildren());
+        nodes.put(CREATES_AND_UPDATES, createBenchmarkNodeChildren());
+
+        return (ObjectNode) JsonNodeFactory.instance.objectNode().setAll(nodes);
     }
 
-    private static ObjectNode createBenchmarkNode(@Nonnull final ObjectNode syncNode,
-                                                  @Nonnull final String benchmark) {
+    private static ObjectNode createBenchmarkNodeChildren() {
+
         final ObjectNode newBenchmarkNode = JsonNodeFactory.instance.objectNode();
-        newBenchmarkNode.set(EXECUTION_TIMES, JsonNodeFactory.instance.arrayNode());
-        newBenchmarkNode.set(AVERAGE, JsonNodeFactory.instance.numberNode(0));
-        newBenchmarkNode.set(DIFF, JsonNodeFactory.instance.numberNode(0));
-
-        syncNode.set(benchmark, newBenchmarkNode);
-        return syncNode;
+        newBenchmarkNode.set(SYNC_TOTAL_DURATION, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(TOTAL_CTP_EXCHANGE_DURATION, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(AVG_CTP_EXCHANGE_DURATION, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(NUMBER_OF_REQUESTS, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(NUMBER_OF_POSTS, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(NUMBER_OF_GETS, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(NUMBER_OF_CREATES, JsonNodeFactory.instance.numberNode(0));
+        newBenchmarkNode.set(NUMBER_OF_UPDATES, JsonNodeFactory.instance.numberNode(0));
+        return newBenchmarkNode;
     }
-
-    static double calculateDiff(@Nonnull final String version,
-                                @Nonnull final String sync,
-                                @Nonnull final String benchmark,
-                                final double average) throws IOException {
-        final JsonNode rootNode = new ObjectMapper().readTree(getFileContent(BENCHMARK_RESULTS_FILE_PATH));
-        return calculateDiff(rootNode, version, sync, benchmark, average);
-    }
-
-    private static double calculateDiff(@Nonnull final JsonNode originalRoot,
-                                @Nonnull final String version,
-                                @Nonnull final String sync,
-                                @Nonnull final String benchmark,
-                                final double average) {
-        return getLatestVersionName(originalRoot, version)
-            .map(latestVersionName -> originalRoot.get(latestVersionName)
-                                                  .get(sync)
-                                                  .get(benchmark)
-                                                  .get(AVERAGE))
-            .map(latestAverageNode -> average - latestAverageNode.asDouble())
-            // if there is no latest version (meaning this is the first benchmark for this module)
-            // then return a 0 diff.
-            .orElse(0.0);
-    }
-
-    private static Optional<String> getLatestVersionName(@Nonnull final JsonNode originalRoot,
-                                                         @Nonnull final String currentVersionName) {
-        return toStream(originalRoot.fieldNames()).reduce((firstVersion, secondVersion) ->
-            !currentVersionName.equals(secondVersion) ? secondVersion : firstVersion);
-    }
-
 
     private static String getFileContent(@Nonnull final String path) throws IOException {
         final byte[] fileBytes = Files.readAllBytes(Paths.get(path));
@@ -172,5 +175,8 @@ public class BenchmarkUtils {
 
     private static void writeToFile(@Nonnull final String content, @Nonnull final String path) throws IOException {
         Files.write(Paths.get(path), content.getBytes(UTF8_CHARSET));
+    }
+
+    private BenchmarkUtils() {
     }
 }
